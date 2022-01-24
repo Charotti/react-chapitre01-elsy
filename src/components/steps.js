cd3761fac84e1e26f05a2a1b583b33198e0bbdd7
unknown <clamy6@yahoo.fr>
2022-01-24 17:23:02 +0100

    exercice03

--- src/components/steps.js
+++ src/components/steps.js
@@ -5,7 +5,7 @@ class Steps extends React.Component {
   render() {
     return (
       <div className="box col-sm3 col-6">
-        <span className="material-icons">directions_walk</span>
+        <span className={styles["material-icons"]}>directions_walk</span>
       </div>
     );
   }
