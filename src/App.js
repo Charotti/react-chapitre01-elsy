a0bf161a3838373a397d76ff36292d2268905ec8
unknown <clamy6@yahoo.fr>
2022-01-24 16:18:30 +0100

    exercice03

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Steps from "./components/steps.js"
 
 const tempMin= -20;
 const tempMax= 40;
@@ -13,8 +14,9 @@ class App extends React.Component {
   render() {
     return (
       <div 
-         className="container-fluid">
+         className="container-fluid ">
            <div class row>
+             <Steps></Steps>
              <p>heart : 80</p>
              <p>Temperature : -20</p>
              <p>Steps : 0</p>
@@ -23,5 +25,4 @@ class App extends React.Component {
     );
   }
 }
-
 export default App;
--- src/components/steps.js
+++ src/components/steps.js
@@ -1,8 +1,13 @@
 import React from "react";
+import styles from "./steps.module.css";
 
-class Steps extends React.component {
-    render() {
-        return <Steps></Steps>
-    }
+class Steps extends React.Component {
+  render() {
+    return (
+      <div className="box col-sm3 col-6">
+        <span className="material-icons">directions_walk</span>
+      </div>
+    );
+  }
 }
 export default Steps;
